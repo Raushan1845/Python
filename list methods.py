88e292a13c5048f109f5107fd865427a30f43cd7
Raushan Kumar <123895684+Raushan1845@users.noreply.github.com>
2026-05-29 12:15:31 +0530

    Create list methods.py

---
+++ list methods.py
@@ -0,0 +1,29 @@
+
+l1 = [6,8,2]  # decleration
+l2 = [2,"raushan","r"]  # decleration
+
+print(l1[1])
+
+# list methods
+
+l1.sort()
+print(l1)
+
+
+l1.reverse()
+print(l1)
+
+l1.append("ok")
+print(l1)
+
+l1.insert(2,5)    # inserted 5 at 2th index
+print(l1)
+
+
+l1.pop(0)
+print(l1)
+
+l1.remove(2)    # remove  2 from the list
+print(l1)
+
+print(l1.count(5))
