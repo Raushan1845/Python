37afde42ca6e29b4a5cf27ea37f6e108dcebc7e3
Raushan Kumar <123895684+Raushan1845@users.noreply.github.com>
2026-05-29 11:49:42 +0530

    Create String functions.py

---
+++ String functions.py
@@ -0,0 +1,19 @@
+#string and its functions
+
+s = "Raushan Kumar"  # decleration
+print(s[2:4])  # string slicing to represent part of text  , it is immutable
+ 
+# functions in string
+print(len(s))
+print(s.endswith("n"))
+print(s.startswith("a"))
+print(s.lower())
+print(s.upper())
+print(s.find("an"))
+print(s.replace("han","anz"))
+print(s.count("a"))
+
+
+
+
+
