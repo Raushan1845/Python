625f2b279121149539dfce9cc03298d92f47e70d
Raushan Kumar <123895684+Raushan1845@users.noreply.github.com>
2026-05-30 22:34:53 +0530

    Create binary search .py

---
+++ binary search .py
@@ -0,0 +1,24 @@
+n = int(input("Enter number of elements: "))
+
+arr = list(map(int, input("Enter sorted elements: ").split()))
+
+key = int(input("Enter element to search: "))
+
+low = 0
+high = n - 1
+found = False
+
+while low <= high:
+    mid = (low + high) // 2
+
+    if arr[mid] == key:
+        print("Element found at index", mid)
+        found = True
+        break
+    elif key < arr[mid]:
+        high = mid - 1
+    else:
+        low = mid + 1
+
+if not found:
+    print("Element not found")
