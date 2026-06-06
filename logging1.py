7892f7fb661e24b9811e28882612e1d48f109cc9
Raushan Kumar <123895684+Raushan1845@users.noreply.github.com>
2026-06-06 11:55:54 +0530

    Create logging1.py

---
+++ logging1.py
@@ -0,0 +1,10 @@
+import logging
+
+logging.basicConfig(level =logging.INFO)
+
+print("this is first statement")
+logging.debug("this will show debug details")
+logging.info("this will display info")
+logging.warning("this will show warning")
+logging.error("this will show error")
+logging.critical("this will show critical error or failuare")
